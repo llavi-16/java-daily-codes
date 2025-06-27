6e40aa5033fa52fc2efe9a7b1d45e8c1d813eeab
llavi-16 <pallavisingh16th@gmail.com>
2025-06-27 11:14:39 +0530

    Add With_return_with_parameter class to demonstrate a method with parameters and return value

---
+++ day_02/With_return_with_parameter.java
@@ -0,0 +1,10 @@
+public class With_return_with_parameter {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        int sum = add(5, 10);
+        System.out.println(sum);
+    }
+    
+}
