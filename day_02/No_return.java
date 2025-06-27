45a102caf7e9fd35299f7288a7547d448a9bec29
llavi-16 <pallavisingh16th@gmail.com>
2025-06-27 10:58:43 +0530

    Add No_return class to demonstrate a method with no return type

---
+++ day_02/No_return.java
@@ -0,0 +1,12 @@
+//this takes no return type
+//it is used to perform an action without returning any value
+// File: day_02/No_return.java
+public class No_return {
+    public static void greet(String name){
+        System.out.println("Hello, ṇ"+name);
+    }
+    public static void main(String[] args){
+        greet("John");
+    }
+    
+}
