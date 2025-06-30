c68b574b87d390d84b1273325f778fce2052e2cf
llavi-16 <pallavisingh16th@gmail.com>
2025-06-30 10:12:21 +0530

    Add ArrayCreation class to demonstrate array initialization and printing

---
+++ day_03/ArrayCreation.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class ArrayCreation{
+    public static void main(String[] args){
+        int[] arr = new int[5];//(Dynamic), default value is 0.
+        int[] prime = {2,3,5,7,9};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(prime));
+    }
+    
+}
