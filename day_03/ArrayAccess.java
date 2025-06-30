2f93b3e92b28c9fc3706640a524c57d91c257b55
llavi-16 <pallavisingh16th@gmail.com>
2025-06-30 10:42:44 +0530

    Add ArrayAccess class to demonstrate array element access and modification

---
+++ day_03/ArrayAccess.java
@@ -0,0 +1,9 @@
+public class ArrayAccess {
+    public static void main(String[] args){
+        int[] arr= {10,20,30,40,50};
+        System.out.println(arr[2]);
+        arr[2] = 99;
+        System.out.println(arr[2]);
+
+    }
+}
