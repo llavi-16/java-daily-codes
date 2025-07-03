cf6fd1e9a12b3227bdddae498ce70287a366d399
llavi-16 <pallavisingh16th@gmail.com>
2025-07-03 11:03:41 +0530

    Add Figure class to calculate areas of circle, rectangle, and triangle

---
+++ day_06/Figure.java
@@ -0,0 +1,24 @@
+public class Figure {
+    public double area(double radius) {
+        return Math.PI * radius * radius; // Area of circle
+    }
+
+    public double area(double length, double width) {
+        return length * width; // Area of rectangle
+    }
+
+    public double area(double base, double height, boolean isTriangle) {
+        if (isTriangle) {
+            return 0.5 * base * height; // Area of triangle
+        }
+        return 0; // Default case if not a triangle
+    }
+
+    public static void main(String[] args) {
+        Figure figure = new Figure();
+        System.out.println("Area of Circle: " + figure.area(5));
+        System.out.println("Area of Rectangle: " + figure.area(4, 6));
+        System.out.println("Area of Triangle: " + figure.area(3, 4, true));
+    }   
+    
+}
