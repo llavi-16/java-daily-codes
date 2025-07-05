047be3e726d7de2b2e00681b79302aed6eb1c986
llavi-16 <pallavisingh16th@gmail.com>
2025-07-05 10:30:42 +0530

    Add Excl and Excl1 classes to demonstrate exception handling in Java

---
+++ day_08/Excl.java
@@ -0,0 +1,10 @@
+class Excl 
+{
+  public static void main(String args[]) 
+  {
+    int d = 0;
+    int a = 42 / d;
+    System.out.println(a);
+    //This will never be printed because of the exception.
+  }
+}
