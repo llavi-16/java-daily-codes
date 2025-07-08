a0717b0d0f5ef6d047eeb2cd5478650bf5d79612
llavi-16 <pallavisingh16th@gmail.com>
2025-07-08 10:54:56 +0530

    Add ParseExample class to demonstrate parsing strings to integers in Java

---
+++ day_10/ParseExample.java
@@ -0,0 +1,8 @@
+public class ParseExample{
+    public static void main(String[] args){
+        String s = "123";
+        int num = Integer.parseInt(s);
+        System.out.println("The number is: " + num*2);
+    }
+
+}
