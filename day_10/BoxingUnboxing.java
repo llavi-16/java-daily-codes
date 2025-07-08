935a910b934caf0f1dd3b968e4869f5a4d11fbfb
llavi-16 <pallavisingh16th@gmail.com>
2025-07-08 11:07:47 +0530

    Add BoxingUnboxing class to demonstrate boxing and unboxing in Java

---
+++ day_10/BoxingUnboxing.java
@@ -0,0 +1,9 @@
+public class BoxingUnboxing{
+    public static void main(String[] args){
+        int a =50;
+        Integer obj = a; // Boxing: converting primitive to object
+        int b = obj; // Unboxing: converting object to primitive
+        System.out.println("object: "+obj);
+        System.out.println("primitive: "+b);
+    }
+}
